6c447478998e2b888707313e64147f4ee3ba007a
Abbos <allanazorovabbos@gmail.com>
2022-11-04 21:49:02 +0500

    phone image input add

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,6 +24,35 @@
                                 @enderror
                             </div>
                         </div>
+{{-- image input --}}
+
+<div class="row mb-3">
+    <label for="name" class="col-md-4 col-form-label text-md-end">image</label>
+
+    <div class="col-md-6">
+        <input id="name" type="file" class="form-control @error('name') is-invalid @enderror" name="traning_image" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+        @error('name')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+</div>
+                             {{-- phone inout --}}
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">phone</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="number" class="form-control @error('name') is-invalid @enderror" name="phone" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
@@ -83,6 +112,10 @@
                                     </span>
                                 @enderror
                             </div>
+
+
+
+
                         </div>
 
                         <div class="row mb-3">
@@ -92,6 +125,7 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
